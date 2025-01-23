63b87f8223dedaab8b199db1d3be8d88c78e32f2
Wahab422 <127966947+Wahab422@users.noreply.github.com>
2025-01-23 13:49:12 +0500

    Follow up

---
+++ src/main.js
@@ -0,0 +1,75 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import fragment from './shaders/fragment.glsl';
+import vertex from './shaders/vertex.glsl';
+
+export default class Sketch {
+  constructor(options) {
+    this.time = 0;
+    this.container = options.dom;
+
+    this.width = this.container.offsetWidth;
+    this.height = this.container.offsetHeight;
+
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(75, this.width / this.height, 0.1, 1000);
+    this.camera.position.z = 3;
+
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.container.appendChild(this.renderer.domElement);
+
+    this.controls = new OrbitControls(this.camera, this.container);
+    this.controls.enableDamping = true;
+    this.controls.dampingFactor = 0.05;
+
+    this.resize();
+    this.setupResize();
+    this.addObject();
+    this.render();
+  }
+
+  addObject() {
+    this.geometry = new THREE.PlaneGeometry(2, 2, 140, 140)
+    this.material = new THREE.MeshNormalMaterial()
+    // Fixed ShaderMaterial with corrected syntax
+    this.material = new THREE.ShaderMaterial({
+      uniforms: {
+        time: { value: 0 }
+      },
+      side: THREE.DoubleSide,
+      wireframe: true,
+      fragmentShader: fragment,
+      vertexShader: vertex,
+    });
+
+    this.mesh = new THREE.Mesh(this.geometry, this.material);
+    this.scene.add(this.mesh);
+  }
+
+
+  setupResize() {
+    window.addEventListener('resize', this.resize.bind(this));
+  }
+
+  resize() {
+    this.width = this.container.offsetWidth;
+    this.height = this.container.offsetHeight;
+
+    this.renderer.setSize(this.width, this.height);
+    this.camera.aspect = this.width / this.height;
+    this.camera.updateProjectionMatrix();
+  }
+
+  render() {
+    this.time += 0.5;
+    this.material.uniforms.time.value = this.time
+    this.controls.update();
+    this.renderer.render(this.scene, this.camera);
+    window.requestAnimationFrame(this.render.bind(this));
+  }
+}
+
+new Sketch({
+  dom: document.getElementById('canvas-wrapper'),
+});
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import topLevelAwait from 'vite-plugin-top-level-await'
+import glsl from 'vite-plugin-glsl';
 
 export default defineConfig({
-  plugins: [topLevelAwait()],
+  plugins: [topLevelAwait(), glsl()],
   base: './',
 })
